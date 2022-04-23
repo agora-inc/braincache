8fa026d5bfc7c353ff9b182d3a504a6bade633c2
Max Taylor-Davies <maxtaylordavies@gmail.com>
2022-04-23 17:33:26 +0100

    adds more content

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,16 +1,58 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { motion, useTransform, useViewportScroll } from "framer-motion";
 
+import { AcademicMainCopy, BusinessMainCopy } from "./constants";
 import { useMediaQuery } from "./useMediaQuery";
 import { ReactComponent as Logo } from "./logo.svg";
 import "./App.css";
 
+type ViewerType = "academic" | "business";
+
 const App = () => {
+  const [viewerType, setViewerType] = useState<ViewerType>("academic");
   const onMobile = useMediaQuery("(max-width: 600px)");
 
   const classname = (name: string): string => {
     return `${name}${onMobile ? " mobile" : ""}`;
   };
+
+  const { scrollYProgress } = useViewportScroll();
+  const headerOpacity = useTransform(scrollYProgress, [0, 0.25], [1, 0]);
+
+  const mainCopy = (
+    <>
+      {(viewerType === "academic" ? AcademicMainCopy : BusinessMainCopy).map(
+        (point) => (
+          <h3 className={classname("secondary-text")}>{point}</h3>
+        )
+      )}
+    </>
+  );
+
+  const typeSelector = (
+    <motion.div
+      className={classname("type-selector")}
+      whileHover={{ scale: 1.02 }}
+    >
+      Are you{" "}
+      <span
+        className={`type-selector-option${
+          viewerType === "academic" ? " active" : ""
+        }`}
+        onClick={() => setViewerType("academic")}
+      >
+        an academic
+      </span>{" "}
+      <span
+        className={`type-selector-option${
+          viewerType === "business" ? " active" : ""
+        }`}
+        onClick={() => setViewerType("business")}
+      >
+        a business
+      </span>
+    </motion.div>
+  );
 
   return (
     <motion.div
@@ -25,52 +67,41 @@ const App = () => {
           href="/"
           whileHover={{ scale: 1.06 }}
           whileTap={{ scale: 0.95 }}
+          style={{ opacity: headerOpacity }}
         >
           <Logo className={classname("header-logo")} />
           <div className={classname("header-name")}>Braincache</div>
         </motion.a>
+        {!onMobile && typeSelector}
         <motion.a
           className={classname("coming-soon")}
           href="https://airtable.com/shrhGt0giRGAY3yxA"
-          animate={{ rotate: "2deg" }}
+          // animate={{ rotate: "2deg" }}
           whileHover={{ scale: 1.06 }}
           whileTap={{ scale: 0.95 }}
+          style={{ opacity: headerOpacity }}
         >
-          Coming soon
+          Join the waitlist →
         </motion.a>
       </header>
       <div className={classname("main-content")}>
+        {onMobile && typeSelector}
         <motion.h1
           className={classname("main-text")}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5 }}
-          // style={{
-          //   marginBottom: onMobile ? 30 : 50,
-          //   fontSize: onMobile ? 28 : 54,
-          //   lineHeight: onMobile ? "26px" : "50px",
-          // }}
         >
           Connecting high-impact businesses to relevant experts in academia
         </motion.h1>
-        <motion.h3
-          className={classname("secondary-text")}
+        <motion.div
+          className={classname("secondary-text-container")}
           initial={{ opacity: 0 }}
-          animate={{ opacity: 0.6 }}
+          animate={{ opacity: 1 }}
           transition={{ duration: 1 }}
         >
-          Are you an academic interested in exploring microconsulting
-          opportunities?
-        </motion.h3>
-        <motion.h3
-          className={classname("secondary-text")}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 0.6 }}
-          transition={{ duration: 1.5 }}
-        >
-          Are you a business looking to leverage world-leading academic
-          expertise?
-        </motion.h3>
+          {mainCopy}
+        </motion.div>
         <motion.a
           className={classname("main-button")}
           href="https://airtable.com/shrhGt0giRGAY3yxA"
