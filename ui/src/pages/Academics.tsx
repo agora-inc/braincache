0847757193a0a0d7e16e1e881c03cd198dd209c2
Max Taylor-Davies <maxtaylordavies@gmail.com>
2022-05-31 19:06:34 +0100

    adds steps content + updates background

--- ui/src/pages/Academics.tsx
+++ ui/src/pages/Academics.tsx
@@ -3,7 +3,7 @@ import { motion, useTransform, useViewportScroll } from "framer-motion";
 
 import { useMediaQuery } from "../useMediaQuery";
 import { ReactComponent as Logo } from "../logo.svg";
-import { FeaturesCarousel } from "../components/FeaturesCarousel";
+import { FeaturesCarousel } from "../components/FeaturesCarousel/FeaturesCarousel";
 
 import "../App.css";
 
@@ -14,33 +14,8 @@ export const Academics = () => {
     return `${name}${onMobile ? " mobile" : ""}`;
   };
 
-  //   const { scrollYProgress } = useViewportScroll();
-  //   const headerOpacity = useTransform(scrollYProgress, [0, 0.25], [1, 0]);
-
-  //   const typeSelector = (
-  //     <motion.div
-  //       className={classname("type-selector")}
-  //       whileHover={{ scale: 1.02 }}
-  //     >
-  //       Are you{" "}
-  //       <span
-  //         className={`type-selector-option${
-  //           viewerType === "academic" ? " active" : ""
-  //         }`}
-  //         onClick={() => setViewerType("academic")}
-  //       >
-  //         an academic
-  //       </span>{" "}
-  //       <span
-  //         className={`type-selector-option${
-  //           viewerType === "business" ? " active" : ""
-  //         }`}
-  //         onClick={() => setViewerType("business")}
-  //       >
-  //         a business
-  //       </span>
-  //     </motion.div>
-  //   );
+  const { scrollYProgress } = useViewportScroll();
+  const headerOpacity = useTransform(scrollYProgress, [0, 0.25], [1, 0]);
 
   return (
     <motion.div
@@ -49,7 +24,7 @@ export const Academics = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.8 }}
       style={{
-        backgroundImage: `url(${window.location.origin}/images/background-3.png)`,
+        backgroundImage: `url(${window.location.origin}/images/background.png)`,
       }}
     >
       <header className={"App-header"}>
@@ -58,12 +33,11 @@ export const Academics = () => {
           href="/"
           whileHover={{ scale: 1.06 }}
           whileTap={{ scale: 0.95 }}
-          //   style={{ opacity: headerOpacity }}
+          style={{ opacity: headerOpacity }}
         >
           <Logo className={classname("header-logo")} />
           <div className={classname("header-name")}>BrainAPI</div>
         </motion.a>
-        {/* {!onMobile && typeSelector} */}
         <div>
           <motion.a className="switch-page-link" href="/companies">
             For companies
@@ -71,17 +45,14 @@ export const Academics = () => {
           <motion.a
             className={classname("coming-soon")}
             href="https://airtable.com/shrhGt0giRGAY3yxA"
-            // animate={{ rotate: "2deg" }}
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
-            //   style={{ opacity: headerOpacity }}
           >
             Join the waitlist →
           </motion.a>
         </div>
       </header>
       <div className={classname("main-content")}>
-        {/* {onMobile && typeSelector} */}
         <motion.h1
           className={classname("main-text")}
           initial={{ opacity: 0 }}
@@ -111,10 +82,9 @@ export const Academics = () => {
         <motion.a
           className={classname("main-button")}
           href="https://airtable.com/shrhGt0giRGAY3yxA"
-          initial={{ opacity: 0, rotate: "-1deg" }}
+          initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
-            rotate: "-3deg",
             transition: { duration: 2 },
           }}
           whileHover={{
@@ -126,7 +96,7 @@ export const Academics = () => {
           Join the waitlist →
         </motion.a>
       </div>
-      <FeaturesCarousel mobile={onMobile} />
+      <FeaturesCarousel mobile={onMobile} audience={"academics"} />
     </motion.div>
   );
 };
