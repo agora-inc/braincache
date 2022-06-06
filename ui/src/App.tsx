4230aa24351fbd25ddd78394c1decd8010db8c27
Max Taylor-Davies <maxtaylordavies@gmail.com>
2022-06-06 16:10:37 +0100

    removes old pages/ and components/ directories, and old App.tsx

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,24 +1,38 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import React from 'react'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Academics } from "./pages/Academics";
-import { Companies } from "./pages/Companies";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+import {ResponsiveProvider} from './contexts/ResponsiveContext'
+
+import IndexPage from "./Pages/IndexPage";
+import HomePage from "./Pages/HomePage";
+import NoPage from "./Pages/404Page";
+import ExpertLandingPage from "./Pages/ExpertLandingPage";
+import CompanyLandingPage from "./Pages/CompanyLandingPage";
+
+
+library.add(faCoffee)
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/academics" element={<Academics />} />
-        <Route path="/companies" element={<Companies />} />
-        <Route path="/" element={<Navigate to="/companies" replace={true} />} />
-      </Routes>
-    </Router>
-  );
-};
+
+    <ResponsiveProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<IndexPage />}>
+            <Route index element={<HomePage />} />
+            <Route path="experts" element={<ExpertLandingPage />} />
+            <Route path="companies" element={<CompanyLandingPage />} />
+          </Route>
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ResponsiveProvider>
+
+  )
+}
 
 export default App;
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./New.App";
+import App from "./App";
 
-const app_container = document.getElementById('root')!;
+const app_container = document.getElementById("root")!;
 const root = ReactDOM.createRoot(app_container);
 
 root.render(<App />);
